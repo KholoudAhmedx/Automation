f93d82b3f85674f1feb58a56f1ec06f8b5a27aff
KholoudAhmedx <kholoudahmedmuhamed4@gmail.com>
2026-01-11 17:54:12 +0200

    Document the steps to generate test cases

--- Web/Bypass403.py
+++ Web/Bypass403.py
@@ -9,16 +9,44 @@ The goal is to automate testing all approaches to bypass it including but not li
 5. Other headers such as Content-Type -- done 
 6. Append IP addresses -- next step
 7. Optimize code. -- next step
-8. ASN/Geo
-9. Refererer Headers
-10. Test on valid valid scenarios. -- next step.
-12. Send them via burp? -- done
-13. Tailor the args to be more customizable (e.g., if the user does not want to add these lists);--> next step.
-14. Try to conquer it if some of the methods or techniques does not make difference. -- next step.
-15. Add auth header and see if it makes difference (403 test instead of 401) -- done
-16. Display the request arch (to make sure it is structured correctly). -- next step.
-17 leveraging `curl` command to perform requests with different techniques.
+8. Refererer Headers
+9. Test on valid valid scenarios. -- next step.
+10. Send them via burp? -- done
+11. Tailor the args to be more customizable (e.g., if the user does not want to add these lists);--> next step.
+12. Try to conquer it if some of the methods or techniques does not make difference. -- next step.
+13. Add auth header and see if it makes difference (403 test instead of 401) -- done
+14. Display the request arch (to make sure it is structured correctly). -- next step.
+15 leveraging `curl` command to perform requests with different techniques.
 '''
+
+'''
+Shift the model from looping over all cases to "generating testcases and then executing them"
+Test case: 
+    ## Method = GET
+    ## User-Agent = X
+    ## Content-Type = Y
+    ## Forwarded-Header = Z
+    ## IP = 1.2.3.4
+    ## Auth = present/ absent
+
+## Design optimization:
+  ## Questions to ask myself:
+    ## 1. Which dimensions should be paired (e.g., forwarder headers and IPs)
+    ## 2. Do I need to test every combination?
+    ## 3. What varies, what stays constant? (e.g., URL usually constant)
+    ## 4. What should be a single test case? 
+    ## 5. What fields does it have or should have? What are optional fields? 
+
+  ## Design considerations:
+    ## 1. Execution logic remain clean and flat; One loop for generating test cases, one for executing them. 
+    ## 2. Reduce combinations without loosing coverage.
+
+  ## Test case design:
+    ## A test case is a one concrete HTTP request .
+    ## <Method, Endpoint, Authentication/NoAuth, Extra-headers (User-Agent, Content-Type, Forwarded-Header + IP)>
+
+'''
+
 import requests
 import sys
 
