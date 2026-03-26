e4f343004357238a7baddf754503f819bcf2c989
Kholoud Ahmed <20201700237@cis.asu.edu.eg>
2026-03-26 09:41:59 +0200

    Brainstorming how auth would be handled in the script

--- Web/Authentication-and-Session-Management/Auth.py
+++ Web/Authentication-and-Session-Management/Auth.py
@@ -1,1 +1,6 @@
-
+# Automate bypassing all types of authentication 
+# Types: HTTP Basic Auth, HTTP Digest Auth, 
+# Inital idea
+# 1. Detect type of authentication from response header 
+# 2. Bruteforce values of possible
+# 3. Bypassing techniques
