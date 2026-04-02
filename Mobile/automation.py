dfd92264c69e7bb76f492b6c23ecf86cf37e725c
Kholoud Ahmed <20201700237@cis.asu.edu.eg>
2026-04-02 08:15:59 +0200

    Create automation.py

---
+++ Mobile/automation.py
@@ -0,0 +1,8 @@
+'''
+>[!Note] This tool is supposed to do the repetitive task i keep doing when conducting penetration testing. </br>
+# Ideas Logs
+1. Create a script to automate the process of building an apk, zipaligning it and signing it
+  1. Start by very basic steps
+  2. Consider possible errors that may appear
+  3. Test it repeatdly
+'''
