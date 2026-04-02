6daa6f68457bd341af4cbfc1af67fcd4a708e770
Kholoud Ahmed <20201700237@cis.asu.edu.eg>
2026-04-02 08:37:28 +0200

    Add a note on decompiling APKs

--- Mobile/automation.py
+++ Mobile/automation.py
@@ -1,5 +1,6 @@
 '''
 >[!Note] This tool is supposed to do the repetitive task i keep doing when conducting penetration testing. </br>
+>[Note] Decompile code with newer jetpack compose package/framework with `--no-res` option in order to execlude resource from update via this command `apktool d quills.apk -o quills --no-res`
 # Ideas Logs
 1. Create a script to automate the process of building an apk, zipaligning it and signing it
   1. Start by very basic steps
